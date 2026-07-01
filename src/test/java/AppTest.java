3a13ee608c62efada5fb3a397bb0950ec0583923
Kirill <kornienko.krll@gmail.com>
2026-07-01 22:47:14 +0400

    fix class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class AppTest {
